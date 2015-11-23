88b26fd21869336091d5a90e685c16993acc2d84
Francis Vargas <fvargas213@gmail.com>
2015-11-23 16:47:58 -0500

    seperated structure, presentation, behaviour & added deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,32 @@
+echo "--get current branch--"
+branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+
+echo "--create temp folder--"
+mkdir ~/Desktop/temp
+
+echo "--copy public dir contents to temp dir--"
+cp -r public/. ~/Desktop/temp
+
+echo "--switch to master branch--"
+git checkout master
+
+echo "--copy from temp and paste to current dir--"
+cp -r ~/Desktop/temp/. .
+
+echo "--remove temp dir from desktop--"
+rm -R ~/Desktop/temp/
+
+echo "--git add--"
+git add -A
+
+echo "--set date to variable--"
+currentDate="$(date)"
+
+echo "--add message to git commit--"
+git commit -m "${currentDate}"
+
+echo "--git push--"
+git push
+
+echo "--switch back to dev branch--"
+git checkout $branch

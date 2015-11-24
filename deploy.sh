a7ff3dcb64c54879ab0832c182b2a5a69a74ffdf
Francis Vargas <fvargas213@gmail.com>
2015-11-24 14:33:16 -0500

    deploy

--- deploy.sh
+++ deploy.sh
@@ -2,16 +2,16 @@ echo "--get current branch--"
 branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 
 echo "--create temp folder--"
-mkdir ~/Desktop/temp
+mkdir ~/Desktop/temp/
 
 echo "--copy public dir contents to temp dir--"
-cp -r . ~/Desktop/temp
+cp -f . ~/Desktop/temp/
 
-echo "--switch to master branch--"
+echo "--switch to gh-pages branch--"
 git checkout gh-pages
 
 echo "--copy from temp and paste to current dir--"
-cp -u ~/Desktop/temp/. .
+cp -f ~/Desktop/temp/. .
 
 echo "--remove temp dir from desktop--"
 rm -f -R ~/Desktop/temp/
@@ -28,5 +28,5 @@ git commit -m "${currentDate}"
 echo "--git push--"
 git push
 
-echo "--switch back to dev branch--"
+echo "--switch back to original branch--"
 git checkout $branch
